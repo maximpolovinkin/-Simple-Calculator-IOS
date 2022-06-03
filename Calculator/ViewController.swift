01abd37ac7ced1f2c13aeadfcef69ff8ef1cb793
maximpolovinkin <polloVinkin@yandex.ru>
2022-06-04 00:41:43 +0300

    Interface and main functions

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,7 +8,89 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
 
+    var operationSign = false
+    var firsNumber: Double = 0
+    var secondNumber: Double = 0
+    var operationId = 0
+    
+    @IBOutlet weak var mainLabel: UILabel!
+     
+    @IBAction func result(_ sender: UIButton) {
+        if operationSign{
+            mainLabel.text?.removeAll()
+            operationSign = false
+            mainLabel.text = mainLabel.text! + String(sender.tag)
+
+            secondNumber = Double(mainLabel.text!)!
+        } else{
+            mainLabel.text = mainLabel.text! + String(sender.tag)
+            firsNumber = Double(mainLabel.text!)!
+        }
+        
+    }
+    @IBAction func clear(_ sender: UIButton) {
+        mainLabel.text?.removeAll()
+    }
+    @IBAction func operations(_ sender: UIButton) {
+        
+        switch sender.tag {
+        case 10:
+            if Int(mainLabel.text!)! < 0 {
+                mainLabel.text = String(abs(Int(mainLabel.text!)!))
+            } else{
+                mainLabel.text = String(-Int(mainLabel.text!)!)
+            }
+            operationSign = true
+            break
+        case 11:
+                mainLabel.text = String((Double(mainLabel.text!)!)/100)
+                operationSign = true
+            break
+        case 12:
+            mainLabel.text = "÷"
+            operationSign = true
+            break
+        case 13:
+            mainLabel.text = "x"
+            operationSign = true
+            break
+        case 14:
+            mainLabel.text = "-"
+            operationSign = true
+            break
+        case 15:
+            mainLabel.text = "+"
+            operationSign = true
+            break
+        case 16:
+            switch operationId{
+            case 12:
+                mainLabel.text = String(firsNumber/secondNumber)
+                break
+            case 13:
+                mainLabel.text = String(firsNumber*secondNumber)
+                break
+            case 14:
+                mainLabel.text = String(firsNumber-secondNumber)
+                break
+            case 15:
+                mainLabel.text = String(firsNumber+secondNumber)
+                break
+            default:
+                mainLabel.text = "+"
+            }
+            operationSign = false
+            firsNumber = Double(mainLabel.text!)!
+            break
+        default:
+            mainLabel.text = "1"
+        }
+        operationId = sender.tag
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
