3a4d7535a057fd3328df4884f6cf167f915a011b
maximpolovinkin <polloVinkin@yandex.ru>
2022-06-04 16:23:27 +0300

    added new styles

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,14 +10,36 @@ import UIKit
 class ViewController: UIViewController {
     
     
-
     var operationSign = false
     var firsNumber: Double = 0
     var secondNumber: Double = 0
     var operationId = 0
     
+
+   
+    @IBOutlet weak var clearButton: UIButton!
     @IBOutlet weak var mainLabel: UILabel!
-     
+    @IBOutlet weak var modulButton: UIButton!
+    @IBOutlet weak var divButton: UIButton!
+    @IBOutlet weak var modButton: UIButton!
+    @IBOutlet weak var multiplicationButton: UIButton!
+    @IBOutlet weak var differenseButton: UIButton!
+    @IBOutlet weak var summaryButton: UIButton!
+    @IBOutlet weak var equalButton: UIButton!
+    @IBOutlet weak var commaButton: UIButton!
+    @IBOutlet weak var zeroButton: UIButton!
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var nineButton: UIButton!
+    
+    
+    
     @IBAction func result(_ sender: UIButton) {
         if operationSign{
             mainLabel.text?.removeAll()
@@ -93,9 +115,31 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        roundButtons()
         // Do any additional setup after loading the view.
     }
 
-
+    func roundButtons(){
+       clearButton.layer.cornerRadius = clearButton.bounds.height / 2
+        modulButton.layer.cornerRadius = modulButton.bounds.height / 2
+        modButton.layer.cornerRadius = modButton.bounds.height / 2
+        divButton.layer.cornerRadius = divButton.bounds.height / 2
+        multiplicationButton.layer.cornerRadius = multiplicationButton.bounds.height / 2
+        differenseButton.layer.cornerRadius = differenseButton.bounds.height / 2
+        summaryButton.layer.cornerRadius = summaryButton.bounds.height / 2
+        equalButton.layer.cornerRadius = equalButton.bounds.height / 2
+        oneButton.layer.cornerRadius = twoButton.bounds.height / 2
+        threeButton.layer.cornerRadius = threeButton.bounds.height / 2
+        fourButton.layer.cornerRadius = fourButton.bounds.height / 2
+        fiveButton.layer.cornerRadius = fiveButton.bounds.height / 2
+        sixButton.layer.cornerRadius = sixButton.bounds.height / 2
+        sevenButton.layer.cornerRadius = sevenButton.bounds.height / 2
+        eightButton.layer.cornerRadius = eightButton.bounds.height / 2
+        nineButton.layer.cornerRadius = nineButton.bounds.height / 2
+        zeroButton.layer.cornerRadius = zeroButton.bounds.height / 2
+        commaButton.layer.cornerRadius = commaButton.bounds.height / 2
+        twoButton.layer.cornerRadius = twoButton.bounds.height / 2
+        
+    }
 }
 
